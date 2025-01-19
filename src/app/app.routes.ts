22b9fe20f8fcc91b0602dcab2867136cab9b262a
Ícaro Miranda <miranda.icarox@gmail.com>
2025-01-19 04:53:50 +0000

    deleting some ununsed components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,7 @@ import { AuthComponent } from './pages/auth/auth.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CreateComponent } from './pages/item/create/create.component';
 import { EditComponent } from './pages/item/edit/edit.component';
-import { ItemCreateComponent } from './pages/item/item-create/item-create.component';
 import { ProductResolver } from './product.resolver';
-import { TesteComponent } from './teste/teste.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -17,22 +15,12 @@ export const routes: Routes = [
     canActivate: [AuthGuard],
   },
   {
-    path: 'teste/:id',
-    component: TesteComponent,
-    canActivate: [AuthGuard],
-  },
-  {
     path: 'item/edit/:id',
     component: EditComponent,
     canActivate: [AuthGuard],
     resolve: {
       product: ProductResolver,
     },
-  },
-  {
-    path: 'item/view/:id',
-    component: ItemCreateComponent,
-    canActivate: [AuthGuard],
   },
   { path: 'auth', component: AuthComponent, canActivate: [LoginGuard] },
   { path: '**', redirectTo: '/home' },
