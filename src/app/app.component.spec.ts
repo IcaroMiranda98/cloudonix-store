96b7f22c574d69d1b19325ce14f8bf79db05bded
Ícaro Miranda <miranda.icarox@gmail.com>
2025-01-28 03:05:16 +0000

    implementing unit test for app-component of main app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,58 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let router: Router;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, RouterTestingModule],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    router = TestBed.inject(Router);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have the 'cloudonix-store' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('cloudonix-store');
+  it(`should have the 'Cloudonix Store' title`, () => {
+    expect(component.title).toEqual('Cloudonix Store');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should return true if the current route is "/auth"', () => {
+    spyOnProperty(router, 'url', 'get').and.returnValue('/auth');
+    expect(component.isLoginPage()).toBeTrue();
+  });
+
+  it('should return false if the current route is not "/auth"', () => {
+    spyOnProperty(router, 'url', 'get').and.returnValue('/home');
+    expect(component.isLoginPage()).toBeFalse();
+  });
+
+  it('should render the header and footer when not on the login page', () => {
+    spyOnProperty(router, 'url', 'get').and.returnValue('/home');
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, cloudonix-store',
-    );
+
+    const header = fixture.nativeElement.querySelector('app-header');
+    const footer = fixture.nativeElement.querySelector('app-footer');
+
+    expect(header).toBeTruthy();
+    expect(footer).toBeTruthy();
+  });
+
+  it('should not render the header and footer when on the login page', () => {
+    spyOnProperty(router, 'url', 'get').and.returnValue('/auth');
+    fixture.detectChanges();
+
+    const header = fixture.nativeElement.querySelector('app-header');
+    const footer = fixture.nativeElement.querySelector('app-footer');
+
+    expect(header).toBeNull();
+    expect(footer).toBeNull();
   });
 });
